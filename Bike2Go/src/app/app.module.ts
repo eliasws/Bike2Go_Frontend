59aa6b59cfad9719bbbedd11d3195628025f990a
Elias <cea09da9@opayq.com>
2016-11-05 03:23:19 +0100

    --

--- Bike2Go/src/app/app.module.ts
+++ Bike2Go/src/app/app.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { IonicApp, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { TabsPage } from '../pages/tabs/tabs';
+
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    TabsPage
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -14,7 +17,8 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    TabsPage
   ],
   providers: []
 })
--- Bike2Go/src/pages/home/home.ts
+++ Bike2Go/src/pages/home/home.ts
@@ -1,85 +1,273 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Slides, NavController } from 'ionic-angular';
 import { Geolocation } from 'ionic-native';
- 
+
 declare var google;
- 
+
 @Component({
   selector: 'home-page',
   templateUrl: 'home.html'
 })
 export class HomePage {
- 
+
   @ViewChild('map') mapElement: ElementRef;
+  @ViewChild('bikeSlider') slider: Slides;
   map: any;
- 
+  slideOptions: any;
+
   constructor(public navCtrl: NavController) {
- 
+    this.slideOptions = {
+      effect: "slide",
+      slidesPerView: 3,
+      zoom: true,
+      zoomMax: 2,
+      centeredSlides: true,
+      loop: true,
+      spaceBetween: 20
+    };
   }
- 
-  ionViewDidLoad(){
-    console.log("TEST");
+
+  ionViewDidLoad() {
     this.loadMap();
   }
- 
-  loadMap(){
- 
+
+  loadMap() {
+
     Geolocation.getCurrentPosition().then((position) => {
- 
+
       let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
- 
+
       let mapOptions = {
         center: latLng,
         zoom: 15,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
+        mapTypeIds: google.maps.MapTypeId.ROADMAP,
+        disableDefaultUI: true,
+
       }
 
       this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+      this.map.setOptions({ styles: this.mapsStyle });
+
+
 
       this.addMyPositionMarker(latLng);
- 
+
     }, (err) => {
       console.log(err);
     });
- 
+
   }
 
-  addMyPositionMarker(pos){
-     let marker = new google.maps.Marker({
-    map: this.map,
-    animation: google.maps.Animation.DROP,
-    position: pos
-  });
- 
-  let content = "<h4>Hallo hier sind wir!</h4>";          
- 
-  this.addInfoWindow(marker, content);
+  addMyPositionMarker(pos) {
+    let marker = new google.maps.Marker({
+      map: this.map,
+      animation: google.maps.Animation.DROP,
+      position: pos
+    });
+
+    let content = "<h4>Hallo hier sind wir!</h4>";
+
+    this.addInfoWindow(marker, content);
   }
 
-  addMarker(){
- 
-  let marker = new google.maps.Marker({
-    map: this.map,
-    animation: google.maps.Animation.DROP,
-    position: this.map.getCenter()
-  });
- 
-  let content = "<h4>Information!</h4>";          
- 
-  this.addInfoWindow(marker, content);
- 
-}
+  addMarker() {
 
-addInfoWindow(marker, content){
- 
-  let infoWindow = new google.maps.InfoWindow({
-    content: content
-  });
- 
-  google.maps.event.addListener(marker, 'click', () => {
-    infoWindow.open(this.map, marker);
-  });
- 
-}
- 
+    let marker = new google.maps.Marker({
+      map: this.map,
+      animation: google.maps.Animation.DROP,
+      position: this.map.getCenter()
+    });
+
+    let content = "<h4>Information!</h4>";
+
+    this.addInfoWindow(marker, content);
+
+  }
+
+  addInfoWindow(marker, content) {
+
+    let infoWindow = new google.maps.InfoWindow({
+      content: content
+    });
+
+    google.maps.event.addListener(marker, 'click', () => {
+      infoWindow.open(this.map, marker);
+    });
+
+  }
+
+  onSlideChanged() {
+    // let currentIndex = this.slider.getActiveIndex();
+    console.log("Current index is", "test");
+  }
+
+
+  mapsStyle = [
+    {
+      "featureType": "all",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "saturation": 36
+        },
+        {
+          "color": "#000000"
+        },
+        {
+          "lightness": 40
+        }
+      ]
+    },
+    {
+      "featureType": "all",
+      "elementType": "labels.text.stroke",
+      "stylers": [
+        {
+          "visibility": "on"
+        },
+        {
+          "color": "#000000"
+        },
+        {
+          "lightness": 16
+        }
+      ]
+    },
+    {
+      "featureType": "all",
+      "elementType": "labels.icon",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "administrative",
+      "elementType": "geometry.fill",
+      "stylers": [
+        {
+          "color": "#000000"
+        },
+        {
+          "lightness": 20
+        }
+      ]
+    },
+    {
+      "featureType": "administrative",
+      "elementType": "geometry.stroke",
+      "stylers": [
+        {
+          "color": "#000000"
+        },
+        {
+          "lightness": 17
+        },
+        {
+          "weight": 1.2
+        }
+      ]
+    },
+    {
+      "featureType": "landscape",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#000000"
+        },
+        {
+          "lightness": 20
+        }
+      ]
+    },
+    {
+      "featureType": "poi",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#000000"
+        },
+        {
+          "lightness": 21
+        }
+      ]
+    },
+    {
+      "featureType": "road.highway",
+      "elementType": "geometry.fill",
+      "stylers": [
+        {
+          "color": "#000000"
+        },
+        {
+          "lightness": 17
+        }
+      ]
+    },
+    {
+      "featureType": "road.highway",
+      "elementType": "geometry.stroke",
+      "stylers": [
+        {
+          "color": "#000000"
+        },
+        {
+          "lightness": 29
+        },
+        {
+          "weight": 0.2
+        }
+      ]
+    },
+    {
+      "featureType": "road.arterial",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#000000"
+        },
+        {
+          "lightness": 18
+        }
+      ]
+    },
+    {
+      "featureType": "road.local",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#000000"
+        },
+        {
+          "lightness": 16
+        }
+      ]
+    },
+    {
+      "featureType": "transit",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#000000"
+        },
+        {
+          "lightness": 19
+        }
+      ]
+    },
+    {
+      "featureType": "water",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#000000"
+        },
+        {
+          "lightness": 17
+        }
+      ]
+    }
+  ]
+
 }
