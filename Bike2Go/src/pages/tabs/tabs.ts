e25272f9ecbd75894efd1612698ef8a4530bbf11
Manuel Spitzl <Manuel Spitzl>
2016-11-05 11:40:10 +0100

    List View Page added

--- Bike2Go/src/pages/tabs/tabs.ts
+++ Bike2Go/src/pages/tabs/tabs.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 import { HomePage } from '../home/home';
 
+import {ListViewPage} from '../list-view/list-view';
+
 @Component({
   templateUrl: 'tabs.html'
 })
@@ -9,7 +11,7 @@ export class TabsPage {
   // this tells the tabs component which Pages
   // should be each tab's root Page
   tab1Root: any = HomePage;
-  tab2Root: any;
+  tab2Root: any = ListViewPage;
 
   constructor() {
 
